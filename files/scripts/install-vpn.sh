5527aed785dd37c89839d2e3906d7b80e6cc8a41
Jill Fiore <contact@lumaeris.com>
2025-08-22 11:44:47 +0300

    fix(script): remove unnecessary line

--- files/scripts/install-vpn.sh
+++ files/scripts/install-vpn.sh
@@ -12,6 +12,5 @@ chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
 cp x-ui/x-ui.sh /usr/bin/x-ui
 cp -f x-ui/x-ui.service /etc/systemd/system/
 mv x-ui/ /usr/local/
-systemctl daemon-reload
 popd > /dev/null
 rm -rf "${dir}"
